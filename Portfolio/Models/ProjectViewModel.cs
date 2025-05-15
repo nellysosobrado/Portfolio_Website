86309235a9c8072c875b4c097f88a4a46da551ea
Nelly <Nelly.sosobrado@gmail.com>
2025-05-15 13:13:30 +0200

    Added ProjectViewModel

---
+++ Portfolio/Models/ProjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Models
+{
+    public class ProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string TechStack { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime CreatedDate { get; set; }
+        public string GithubUrl { get; set; } = "";
+        public string LiveDemoUrl { get; set; } = "";
+    }
+}
