d2d7c20c6919a92a240cc55657707f2d302abeb1
Nelly <Nelly.sosobrado@gmail.com>
2025-05-15 13:21:40 +0200

    Added Project (API) button to navbar

---
+++ Portfolio/Pages/Portfolio.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+
+namespace Portfolio.Pages
+{
+    public class PortfolioModel : PageModel
+    {
+        private readonly HttpClient _http;
+
+        public PortfolioModel(IHttpClientFactory factory)
+        {
+            _http = factory.CreateClient();
+        }
+
+        public List<ProjectViewModel> Projects { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+
+            var apiUrl = "https://localhost:7253/api/projects";
+            var result = await _http.GetFromJsonAsync<List<ProjectViewModel>>(apiUrl);
+
+            if (result != null)
+                Projects = result;
+        }
+    }
+}
