351ce9b47b85e283f29bda34224f781a18f9a93a
Nelly <Nelly.sosobrado@gmail.com>
2025-05-15 22:26:26 +0200

    Created DAL class library, to sort out server related code

--- Portfolio/Models/ContactMessage.cs
+++ Portfolio/Models/ContactMessage.cs
@@ -5,16 +5,9 @@ namespace Portfolio.Models
     public class ContactMessage
     {
         public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        [Required, EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        public string Message { get; set; }
-
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
         public DateTime SentAt { get; set; } = DateTime.Now;
     }
 }
