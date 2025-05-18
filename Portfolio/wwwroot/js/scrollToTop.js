3758d9268b855850bf1fab0752e95d71ed98d22d
nellysosobrado <Nelly.sosobrado@gmail.com>
2025-05-18 16:43:08 +0200

    Implemented top button

---
+++ Portfolio/wwwroot/js/scrollToTop.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const scrollBtn = document.getElementById("scrollToTopBtn");
+
+    if (!scrollBtn) return;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 200) {
+            scrollBtn.style.display = "block";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    });
+
+    scrollBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
